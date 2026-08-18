807b376019e26091f21d4a2e8f7135053d261943
Warren Gates <warren-gates@users.noreply.github.com>
2026-08-18 14:22:18 -0600

    configuration changes

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,9 +8,19 @@ export default defineConfig({
     srcDir: '.',
     manifest: {
         name: 'Tools for Xero',
-        version: '1.0.0-alpha1',
-        description: 'Bookkeeping tools and shortcuts for the Xero accounting web app.',
+        description: 'UI shortcuts and improvements for the Xero accounting web app.',
         permissions: ['storage'],
-        host_permissions: ['https://go.xero.com/*', 'https://reporting.xero.com/*']
+        host_permissions: ['https://go.xero.com/*', 'https://reporting.xero.com/*'],
+        content_security_policy: {
+            extension_pages: "script-src 'self'; object-src 'self';"
+        },
+        browser_specific_settings: {
+            gecko: {
+                id: 'tools-for-xero@purplesquirreltechnology.com',
+                data_collection_permissions: {
+                    required: ['none']
+                }
+            }
+        }
     },
 });
