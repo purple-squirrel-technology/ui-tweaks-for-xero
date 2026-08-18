8aeb341c846e0303b832db61fe9186164e9512a7
Warren Gates <warren-gates@users.noreply.github.com>
2026-08-18 12:36:41 -0600

    added logo SVG and integrated auto-icons module in configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'wxt';
+import {defineConfig} from 'wxt';
 
 export default defineConfig({
-  srcDir: '.',
-  manifest: {
-    name: 'Tools for Xero',
-    description: 'Bookkeeping tools and shortcuts for the Xero accounting web app.',
-    permissions: ['storage'],
-    host_permissions: ['https://go.xero.com/*'],
-  },
+    modules: ['@wxt-dev/auto-icons'],
+    autoIcons: {
+        baseIconPath: 'assets/logo.svg'
+    },
+    srcDir: '.',
+    manifest: {
+        name: 'Tools for Xero',
+        version: '1.0.0-alpha1',
+        description: 'Bookkeeping tools and shortcuts for the Xero accounting web app.',
+        permissions: ['storage'],
+        host_permissions: ['https://go.xero.com/*', 'https://reporting.xero.com/*']
+    },
 });
